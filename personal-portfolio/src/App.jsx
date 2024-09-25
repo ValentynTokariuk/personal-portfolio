e9b173b9de5f285ff20967750a09f2584c0c4a99
ValentynTokariuk <125217445+ValentynTokariuk@users.noreply.github.com>
2024-09-25 15:06:30 +0200

    css update

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -4,31 +4,34 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
+import Home from './Home';
 
 const App = () => {
   return (
     <Router>
       <div className="portfolio">
         <header>
-          <h1>My Portfolio</h1>
+          <h1>Valentyn Tokariuk</h1>
+          <h2>Full Stack Developer</h2>
           <nav>
+            <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </nav>
         </header>
-
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} /> 
-          <Route path="/" element={<Home />} />
-
+          <Route path="*" element={<Home />} /> {/* Catch-all route to avoid 404 errors */}
         </Routes>
 
         <footer>
-          <p>Contact: example@example.com</p>
+          <h3><a href='./Contact'>Contact me</a></h3>
+          
         </footer>
       </div>
     </Router>
