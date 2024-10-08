df6c9a457f4965b26e7248414bd26c83e97d2807
ValentynTokariuk <125217445+ValentynTokariuk@users.noreply.github.com>
2024-10-08 12:22:12 +0200

    Deployment update

--- personal-portfolio/vite.config.js
+++ personal-portfolio/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Ensure relative paths for deployment
-  assetsInclude: ['**/*.key', '**/*.JPG'],  // Proper asset inclusion
-});
+  assetsInclude: ['**/*.key', '**/*.JPG'],
+})
+
